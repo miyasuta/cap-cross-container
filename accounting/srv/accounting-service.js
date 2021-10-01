f47e371201ceeadaee0574d2a08aad78beecbedd
Mio Yasutake <mio.fujita.01@gmail.com>
2021-10-02 06:11:41 +0900

    add $expand handling

--- accounting/srv/accounting-service.js
+++ accounting/srv/accounting-service.js
@@ -1,3 +1,5 @@
+const cds = require("@sap/cds");
+
 module.exports = async function () {
     const MasterPartnersService = await cds.connect.to('MasterPartnersService')
 
@@ -5,8 +7,30 @@ module.exports = async function () {
         return await MasterPartnersService.tx(req).run(req.query);
     })
 
-    this.after('READ', 'Invoices', async (data) => {
-        console.log(data);
-    })
+    this.on('READ', 'Invoices', async (req, next) => {
+        var data = await next();
+        const expand = req.req.query['$expand']
+        if(expand && expand === 'customer') {
+            data = await _getCustomers(data)
+        }
+        return data
+    }) 
 
 }
+
+const _getCustomers = async function (data) {
+    const MasterPartnersService = await cds.connect.to('MasterPartnersService')
+    const ids = data.reduce((curr, d) => {
+        curr.push(d.customer_ID)
+        return curr
+    }, []).filter(d => !!d)
+
+    const qry = SELECT.from('MasterPartnersService.Customers').where('ID in ', ids);
+    const res = await MasterPartnersService.tx().run(qry)
+
+    data.map(d => {
+        d.customer = res.filter(r => r.ID === d.customer_ID)[0]
+    })
+    return data
+} 
+
