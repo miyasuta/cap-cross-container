b7c77d955ff60f3a87a3b4bf13b192d96aea95d0
Mio Yasutake <mio.fujita.01@gmail.com>
2021-10-03 09:45:47 +0900

    modify hdi name

--- accounting/srv/accounting-service.cds
+++ accounting/srv/accounting-service.cds
@@ -1,7 +1,6 @@
 using accounting from '../db/data-model';
-using { MasterPartnersService } from './external/MasterPartnersService';
 
 service AccountingService {
     entity Invoices as projection on accounting.Invoices;
-    entity Customers as projection on MasterPartnersService.Customers;
+    entity Customers as projection on accounting.Customers;
 }
--- accounting/srv/accounting-service.js
+++ accounting/srv/accounting-service.js
@@ -1,12 +1,11 @@
 module.exports = async function () {
-    const MasterPartnersService = await cds.connect.to('MasterPartnersService')
+    // const MasterPartnersService = await cds.connect.to('MasterPartnersService')
 
-    this.on('READ', 'Customers', async(req) => {
-        return await MasterPartnersService.tx(req).run(req.query);
-    })
+    // this.on('READ', 'Customers', async(req) => {
+    //     return await MasterPartnersService.tx(req).run(req.query);
+    // })
 
-    this.after('READ', 'Invoices', async (data) => {
-        console.log(data);
-    })
-
+    // this.after('READ', 'Invoices', async (data) => {
+    //     console.log(data);
+    // })
 }
